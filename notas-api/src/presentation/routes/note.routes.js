161c01e7e69f955a0e52359486f3eea921d4c658
Dayana Hernandez <daydev01@gmail.com>
2026-04-26 23:55:21 -0400

    implementar getById, update y delete para notas

--- notas-api/src/presentation/routes/note.routes.js
+++ notas-api/src/presentation/routes/note.routes.js
@@ -20,6 +20,7 @@ const router = Router();
 // Definir las rutas para las notas  
 router.post("/", authMiddleware, upload.single('image'), noteController.createNote);
 router.get("/", authMiddleware, noteController.getNotesByUserId);
+router.get("/:id", authMiddleware, noteController.getNote);
 router.put("/:id", authMiddleware, upload.single('image'), noteController.updateNote);
 router.delete("/:id", authMiddleware, roleMiddleware(["admin"]), noteController.deleteNote);
 router.post("/:id/share", authMiddleware, noteController.shareNote);
